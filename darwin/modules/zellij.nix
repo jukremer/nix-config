528d43d7652a6ec4541ab7040bfb1b5f5d65e0a5
Jan Kremer <mail@jankremer.eu>
2024-02-21 14:43:33 +0100

    Zellij: fix theme

--- darwin/modules/zellij.nix
+++ darwin/modules/zellij.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       default_shell = "fish";
-      theme = "catppuccin-mocha";
+      theme = "catppuccin-macchiato";
       default_layout = "compact";
       copy_command = "pbcopy";
       layout_dir = "${config.xdg.dataHome}/zellij";
