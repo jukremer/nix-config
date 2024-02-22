d9ed113d635fedb7495516d0ae46be097930a512
Jan Kremer <mail@jankremer.eu>
2024-02-22 15:26:31 +0100

    Neovim: remove junk form cmp

--- darwin/modules/neovim/cmp.nix
+++ darwin/modules/neovim/cmp.nix
@@ -9,57 +9,39 @@
     plugins = {
       luasnip.enable = true;
 
-      lspkind = {
-        enable = true;
-
-        cmp = {
-          enable = true;
-          menu = {
-            nvim_lsp = "[LSP]";
-            nvim_lua = "[api]";
-            path = "[path]";
-            luasnip = "[snip]";
-            buffer = "[buffer]";
-          };
-        };
-      };
-
       nvim-cmp = {
         enable = true;
+        autoEnableSources = true;
 
         snippet.expand = "luasnip";
 
         mapping = {
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
           "<C-e>" = "cmp.mapping.close()";
-          "<Tab>" = {
-            modes = [
-              "i"
-              "s"
-            ];
-            action = "cmp.mapping.select_next_item()";
-          };
-          "<S-Tab>" = {
-            modes = [
-              "i"
-              "s"
-            ];
-            action = "cmp.mapping.select_prev_item()";
-          };
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = {
+            action = "cmp.mapping.select_prev_item()";
+            modes = [
+              "i"
+              "s"
+            ];
+          };
+          "<Tab>" = {
+            action = "cmp.mapping.select_next_item()";
+            modes = [
+              "i"
+              "s"
+            ];
+          };
         };
 
         sources = [
-          { name = "path"; }
           { name = "nvim_lsp"; }
           { name = "luasnip"; }
-          {
-            name = "buffer";
-            # Words from other open buffers can also be suggested.
-            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-          }
+          { name = "path"; }
+          { name = "buffer"; }
         ];
       };
     };
