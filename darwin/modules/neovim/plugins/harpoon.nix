e3f2864329198c01ef3dd86861e4fddb7a066f93
Jan Kremer <mail@jankremer.eu>
2024-02-22 15:39:27 +0100

    Neovim: configure harpoon

--- darwin/modules/neovim/plugins/harpoon.nix
+++ darwin/modules/neovim/plugins/harpoon.nix
@@ -2,19 +2,31 @@
   programs.nixvim = {
     plugins.harpoon = {
       enable = true;
-
       keymapsSilent = true;
 
       keymaps = {
         addFile = "<leader>a";
-        toggleQuickMenu = "<C-e>";
+        toggleQuickMenu = "<leader>0";
+        navNext = "<leader>*";
+        navPrev = "<leader>#";
         navFile = {
-          "1" = "<C-j>";
-          "2" = "<C-k>";
-          "3" = "<C-l>";
-          "4" = "<C-m>";
+          "1" = "<leader>1";
+          "2" = "<leader>2";
+          "3" = "<leader>3";
+          "4" = "<leader>4";
+          "5" = "<leader>5";
+          "6" = "<leader>6";
+          "7" = "<leader>7";
+          "8" = "<leader>8";
+          "9" = "<leader>9";
         };
       };
+    };
+    plugins.which-key.registrations = {
+      "<leader>a" = "Add file to Harpoon";
+      "<leader>0" = "Open Harpoon menu";
+      "<leader>*" = "Go to next Harpoon file";
+      "<leader>#" = "Go to previous Harpoon file";
     };
   };
 }
