697706a15a04264b1d4fb916b362c0fdebc55229
Jan Kremer <mail@jankremer.eu>
2024-02-26 17:29:46 +0100

    Neovim: reenable which-key to match kickstart

--- modules/neovim/plugins/default.nix
+++ modules/neovim/plugins/default.nix
@@ -9,12 +9,15 @@
     ./telescope.nix
     ./treesitter.nix
     ./typst.nix
+    ./which-key.nix
   ];
 
   programs.nixvim = {
+    # Set colorscheme
     colorschemes.catppuccin.enable = true;
     colorschemes.catppuccin.flavour = "macchiato";
 
+    # Plugins that need no/little configuration
     plugins = {
       nvim-autopairs.enable = true;
       indent-blankline.enable = true;
--- modules/neovim/plugins/which-key.nix
+++ modules/neovim/plugins/which-key.nix
@@ -1,6 +1,6 @@
 {
   programs.nixvim.plugins.which-key = {
     enable = true;
-    ignoreMissing = false;
+    # ignoreMissing = false;
   };
 }
