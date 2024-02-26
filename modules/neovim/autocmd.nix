a1b36c5bc01a8acfc3f7605c72961f688ffe2283
Jan Kremer <mail@jankremer.eu>
2024-02-26 17:24:38 +0100

    Neovim: update autoCmd to match kickstart

--- modules/neovim/autocmd.nix
+++ modules/neovim/autocmd.nix
@@ -1,49 +1,65 @@
 {
-  programs.nixvim.autoCmd = [
+  programs.nixvim = {
 
-    {
-      desc = "Set absolute numbers when entering insert mode";
-      event = "InsertEnter";
-      command = "set norelativenumber";
-    }
-    {
-      desc = "Set relative numbers when leaving insert mode";
-      event = "InsertLeave";
-      command = "set relativenumber";
-    }
-    {
-      desc = "Open file on position of last edit";
-      event = "BufReadPost";
-      command = "silent! normal! '.";
-    }
-    {
-      desc = "Highlight on yank";
-      event = "TextYankPost";
-      callback = {
-        __raw = "function() vim.highlight.on_yank() end";
+    autoGroups = {
+      highlight-yank = {
+        clear = true;
       };
-    }
-    {
-      desc = "Open help in vertical split";
-      event = "FileType";
-      pattern = "help";
-      command = "wincmd L";
-    }
-    {
-      desc = "Remove trailing whitespaces on save";
-      event = "BufWrite";
-      command = "%s/\\s\\+$//e";
-    }
-    {
-      desc = "Enable spellcheck in text files";
-      event = "FileType";
-      pattern = [
-        "tex"
-        "latex"
-        "typst"
-        "markdown"
-      ];
-      command = "setlocal spell spelllang=en,de";
-    }
-  ];
+
+      numbers = {
+        clear = true;
+      };
+
+      default = {
+        clear = true;
+      };
+    };
+
+    autoCmd = [
+
+      # Relative numbers in normal mode, absolute numbers in insert mode
+      {
+        desc = "Set absolute numbers when entering insert mode";
+        event = "InsertEnter";
+        group = "numbers";
+        command = "set norelativenumber";
+      }
+      {
+        desc = "Set relative numbers when leaving insert mode";
+        event = "InsertLeave";
+        group = "numbers";
+        command = "set relativenumber";
+      }
+
+      # Open file on position of last edit
+      {
+        desc = "Open file on position of last edit";
+        event = "BufReadPost";
+        command = "silent! normal! '.";
+      }
+
+      # Highlight on yank
+      {
+        desc = "Highlight on yank";
+        event = "TextYankPost";
+        group = "highlight-yank";
+        callback = {
+          __raw = "function() vim.highlight.on_yank() end";
+        };
+      }
+
+      # Enable spellcheck in text files
+      {
+        desc = "Enable spellcheck in text files";
+        event = "FileType";
+        pattern = [
+          "tex"
+          "latex"
+          "typst"
+          "markdown"
+        ];
+        command = "setlocal spell spelllang=de,en";
+      }
+    ];
+  };
 }
