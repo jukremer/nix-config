92b7d45b9432f08018ae0caaf4cc7dbcaa7dfbda
Jan Kremer <mail@jankremer.eu>
2024-02-26 18:42:45 +0100

    Neovim: remove unnecessary keymaps

--- modules/neovim/cmp.nix
+++ modules/neovim/cmp.nix
@@ -16,10 +16,6 @@
         snippet.expand = "luasnip";
 
         mapping = {
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-e>" = "cmp.mapping.close()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
           "<S-Tab>" = {
             action = "cmp.mapping.select_prev_item()";
